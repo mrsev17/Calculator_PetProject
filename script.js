097f3c32f44a00eeae6d1c4ffc061ce22b5e9a4c
Nick <mr.upgrade3@protonmail.com>
2022-08-26 02:51:13 +0300

    Update styles for adaptive

--- script.js
+++ script.js
@@ -1,42 +1,5 @@
 "use strict";
 
-/* === Screen === */
-
-const screen = document.getElementById("screen");
-
-
-/* === Operators === */
-
-const clearBtn = document.getElementById("clear");
-const plus = document.getElementById("plus");
-const minus = document.getElementById("minus");
-const multiply = document.getElementById("multiply");
-const divide = document.getElementById("divide");
-const percent = document.getElementById("percent");
-const comma = document.getElementById("comma");
-const equal = document.getElementById("equal");
-const plusMin = document.getElementById("plusMinus");
-
-/* === Numbers === */
-
-const one = document.getElementById("one");
-const oneClickTwo = document.getElementsByClassName("oneClickTwo");
-
-const two = document.getElementById("two");
-const three = document.getElementById("three");
-const four = document.getElementById("four");
-const five = document.getElementById("five");
-const six = document.getElementById("six");
-const seven = document.getElementById("seven");
-const eight = document.getElementById("eight");
-const nine = document.getElementById("nine");
-const zero = document.getElementById("zero");
-
-/* === Work with first input === */
-
-let firstInput = "";
-let secondInput = "";
-
 
 
 
